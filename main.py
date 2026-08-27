c6f3524aca9c9694ebee4122bd618dae0d8a083b
Gemmer2502 <charles.thil2011@gmail.com>
2026-08-28 01:03:35 +0200

    Adding input validation

--- main.py
+++ main.py
@@ -21,30 +21,51 @@ print(Fore.MAGENTA + r"""______         _____
 print(Fore.RESET)
 
 
-print("How many characters do you want ?")
-num_characters = int(input())
+while True :
+    print("How many characters do you want ?")
+    num_characters = input()
 
-print("Do you want only number (1), only letter (2), both (3), number and special characters (4), letter and special characters (5), all (6) ?")
-choice = int(input())
+    isNumber = True
+    try:
+        int(num_characters)
+    except:
+        isNumber = False
 
-if choice == 1:
-    characters = "0123456789"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
-elif choice == 2:
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
-elif choice == 3:
-    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
-elif choice == 4:
-    characters = "0123456789!@#$%^&*()-+"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
-elif choice == 5:
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
-elif choice == 6:
-    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+"
-    password = ''.join(random.choice(characters) for _ in range(num_characters))
+    if isNumber == False or int(num_characters) <= 0: # Is the input a positive number
+        print(Fore.RED + "Must be higher than 0.")
+        print(Fore.RESET)
+    else:
+        break
+
+
+while True:
+    print("Do you want only number (1), only letter (2), both (3), number and special characters (4), letter and special characters (5), all (6) ?")
+    choice = input()
+
+    isNumber = True
+    try:
+        int(choice)
+    except:
+        isNumber = False
+
+    if isNumber == False or int(choice) < 1 or int(choice )> 6: # Is the input a number between 1 and 6
+        print(Fore.RED + "Must be a number between 1 and 6.")
+        print(Fore.RESET)
+    else:
+        break
+
+options = {
+    1: "0123456789",
+    2: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    3: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    4: "0123456789!@#$%^&*()-+?.,;",
+    5: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
+    6: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
+}
+
+characters = options.get(int(choice))
+
+password = ''.join(random.choice(characters) for _ in range(int(num_characters)))
 
 
 with open("password.txt", "w") as f:
