dd1da7372244bf272c22d258e992dd5d2d119378
Gemmer2502 <charles.thil2011@gmail.com>
2026-08-28 17:30:59 +0200

    Made the script way cleaner

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+###-------###
+###Imports###
+###-------###
 from colorama import Fore
 import secrets
 import pyperclip
@@ -8,14 +11,20 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
 
-print(Fore.MAGENTA + r"""__________                          ________
-\______   \_____    ______ ______  /  _____/  ____   ____
- |     ___/\__  \  /  ___//  ___/ /   \  ____/ __ \ /    \
- |    |     / __ \_\___ \ \___ \  \    \_\  \  ___/|   |  \
- |____|    (____  /____  >____  >  \______  /\___  >___|  /
-                \/     \/     \/          \/     \/     \/""")
+###---------###
+###Fonctions###
+###---------###
+def greetings(): # The name of the project and me (the author)
+    print(Fore.MAGENTA + r"""______               _____            
+| ___ \             |  __ \           
+| |_/ /_ _ ___ ___  | |  \/ ___ _ __  
+|  __/ _` / __/ __| | | __ / _ \ '_ \ 
+| | | (_| \__ \__ \ | |_\ \  __/ | | |
+\_|  \__,_|___/___/  \____/\___|_| |_|
+                                      
+                                      """)
 
-print(Fore.MAGENTA + r"""______         _____                                    
+    print(Fore.MAGENTA + r"""______         _____                                    
 | ___ \       |  __ \                                   
 | |_/ /_   _  | |  \/ ___ _ __ ___  _ __ ___   ___ _ __ 
 | ___ \ | | | | | __ / _ \ '_ ` _ \| '_ ` _ \ / _ \ '__|
@@ -24,7 +33,53 @@ print(Fore.MAGENTA + r"""______         _____
         __/ |                                           
        |___/                                            """)
 
-print(Fore.RESET)
+    print(Fore.RESET)
+
+
+def verify_input(value): # To verify if the input is a int
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def error(text): # To look cleaner
+    print(Fore.RED + text)
+    print(Fore.RESET)
+
+
+def success(text): # To look cleaner
+    print(Fore.GREEN + text)
+    print(Fore.RESET)
+
+
+
+def information(text): # To look cleaner
+    print(Fore.YELLOW + text)
+    print(Fore.RESET)
+
+
+###--------------------###
+###Variable/Dictionnary###
+###--------------------###
+options = { # All the possible characters
+    1: "0123456789",
+    2: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    3: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    4: "0123456789!@#$%^&*()-+?.,;",
+    5: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
+    6: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
+}
+
+
+###---------###
+###Main code###
+###---------###
+greetings()
+
+print("What's your master password (a password that you need to remember that will be used to crypt your password)")
+masterPassword = input()
 
 # Verifying if the salt (the file that will be combined with the master password to create the key) exist if not, creating it.
 if os.path.exists("salt.txt"):
@@ -35,8 +90,7 @@ else:
     with open("salt.txt", "wb") as f:
         f.write(salt)
 
-
-# Defining the setting of hashing
+# Defining the setting of crypting
 kdf = Argon2id(
     salt=salt,
     length=32,
@@ -45,129 +99,73 @@ kdf = Argon2id(
     memory_cost=128 * 1024
 )
 
-
-print("What's your master password (a password that you need to remember that will be used to crypt your password)")
-masterPassword = input()
-
-
 # Creating the key to crypt/decrypt
 key = base64.urlsafe_b64encode(kdf.derive(masterPassword.encode()))
 f = Fernet(key)
 
-
-while True: 
+while True: # Main loop
     print("Do you want to see your password (1), to create password (2) or to leave (3)?")
     mode = input()
 
-    # Verifying if the input is valid
-    isNumber = True
-    try:
-        int(mode)
-    except:
-        isNumber = False
+    if verify_input(mode) == False or int(mode) < 1 or int(mode) > 3:
+        error("Must be a number between 1 and 3.")
 
-    if isNumber == False or int(mode) < 1 or int(mode) > 3:
-        print(Fore.RED + "Must be a number between 1 and 3.")
-        print(Fore.RESET)
-    else:     
-
-
-        # Verify if passwords.enc exists. If yes, decrypt and show the password.
+    else:
         if int(mode) == 1:
-            if os.path.exists("passwords.enc") == False:
-                print(Fore.RED + "You don't have password.")
-                print(Fore.RESET)
+            if os.path.exists("passwords.enc") == False: # Does the file exist
+                error("You don't have password.")
+
             else:
                 with open("passwords.enc", "rb") as passFile:
-                    for line in passFile:
-                        print(f.decrypt(line))
+                    for line in passFile: # Read line by line
+                        try:
+                            print(f.decrypt(line))
+                        except:
+                            error("Wrong master password.")
 
-        # Generate a password as the user wants then crypt it and write it in passwords.enc + the service that it s used for
         elif int(mode) == 2:
-            while True :
+            while True:
                 print("How many characters do you want ?")
                 numCharacters = input()
 
-                # Verifying if the input is valid
-                isNumber = True
-                try:
-                    int(numCharacters)
-                except:
-                    isNumber = False
+                if verify_input(numCharacters) == False or int(numCharacters) <= 0:
+                    error("Must be a number higher than 0.")
 
-                if isNumber == False or int(numCharacters) <= 0:
-                    print(Fore.RED + "Must be a number higher than 0.")
-                    print(Fore.RESET)
                 else:
                     break
 
-            # Defining what kind of password the user wants
             while True:
                 print("Do you want only number (1), only letter (2), both (3), number and special characters (4), letter and special characters (5), all (6) ?")
                 choice = input()
 
-                # Verifying if the input is valid
-                isNumber = True
-                try:
-                    int(choice)
-                except:
-                    isNumber = False
-
-                if isNumber == False or int(choice) < 1 or int(choice )> 6: # Is the input a number between 1 and 6
-                    print(Fore.RED + "Must be a number between 1 and 6.")
-                    print(Fore.RESET)
+                if verify_input(choice) == False or int(choice) < 1 or int(choice )> 6:
+                    error("Must be a number between 1 and 6.")
+                    
                 else:
                     break
 
+            characters = options.get(int(choice)) # Set what characters
 
-            # All the possible characters
-            options = {
-                1: "0123456789",
-                2: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                3: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                4: "0123456789!@#$%^&*()-+?.,;",
-                5: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
-                6: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+?.,;",
-            }
-            characters = options.get(int(choice))
+            password = ''.join(secrets.choice(characters) for _ in range(int(numCharacters))) # Create the password
 
-
-            # Creating the password by mixing x times a random letter choosed in the list of characters
-            password = ''.join(secrets.choice(characters) for _ in range(int(numCharacters)))
-
-
-            # To remember which password is for what
             print("For which service is this password ?(website, email...)")
             service = input()
-
-
-            # Easier to write the hash of one variable
             serviceAndPass = service + ": " + password
 
+            success("Your password is: " + password + "\nIt has been saved in passwords.enc.")
 
-            # Show the password at the screen
-            print(Fore.GREEN + "Your password is: " + password + "\nIt has been saved in passwords.enc.")
-            print(Fore.RESET)
+            with open("passwords.enc", "ab") as passFile: # To write the crypted password
+                passFile.write(f.encrypt((serviceAndPass).encode()) + b"\n")
 
-
-            # Hash the password and write the result
-            with open("passwords.enc", "ab") as passFile:
-                passFile.write(f.encrypt(serviceAndPass.encode()) + b"\n")
-
-
-            # Allow the user to copy the password
-            print(Fore.YELLOW + "Do you want to copy it to the clipboard ? (y/n)")
+            information("Do you want to copy it to the clipboard ? (y/n)")
             clipboard = input().lower()
 
             if clipboard == "y":
                 pyperclip.copy(password)
-                print(Fore.GREEN + "Password copied to clipboard.")
-                print(Fore.RESET)
+                success("Password copied to clipboard.")
             else:
-                print(Fore.WHITE + "Password not copied to clipboard.")
-                print(Fore.RESET)
+                information("Password not copied to clipboard")
 
-        # To leave the loop
         elif int(mode) == 3:
             print("Goodbye.")
             break
